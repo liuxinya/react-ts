6b4f8110e07fccef22fd19be422dbdcfb419d777
v_luhuan <v_luhuan@baidu.com>
2020-11-13 16:46:12 +0800

    setCookie 和 delCookie ACG-CSC-Plat-1694
    Change-Id: Iad02501c68e0ead7a400f2c295239634dee88982

--- src/common/helper/cookie.ts
+++ src/common/helper/cookie.ts
@@ -3,9 +3,27 @@
  * 获取cookie
  * @param key 只获取某个key对应的value
  */
+
+import { isObject } from './type';
+
 export function getCookie(key?: string) {
     let cookie = getCookieToObj();
     return key ? cookie[key] : cookie;
+}
+
+export function setCookie<T>(key: string, value: T, ms: number = 0) {
+    const valueStr: string = isObject(value) ? JSON.stringify(value) : String(value);
+    let expires: string = '';
+    if (ms > 0) {
+        let d: Date = new Date();
+        d.setTime(d.getTime() + ms);
+        expires = '; expires=' + d.toUTCString();
+    }
+    document.cookie = key + '=' + escape(valueStr) + expires;
+}
+
+export function delCookie(key: string) {
+    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
 }
 
 function getCookieToObj() {
@@ -14,7 +32,7 @@ function getCookieToObj() {
     let res: cookieObj  = {};
     cookieArr.forEach((item: string) => {
         let temArr = item.split('=');
-        res[temArr[0]] = temArr[1];
+        res[temArr[0].trim()] = unescape(temArr[1]);
     });
     return res;
 }
